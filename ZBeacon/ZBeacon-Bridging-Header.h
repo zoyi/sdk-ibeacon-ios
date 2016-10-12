4a28047fdef4f631401164b4c8bdd228f7ca7915
Sean <sean@zoyi.co>
2016-10-12 15:44:14 +0900

    Generate sample customer id

---
+++ ZBeacon/ZBeacon-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  ZBeacon-Bridging-Header.h
+//  ZBeacon
+//
+//  Created by 이수완 on 2016. 10. 12..
+//  Copyright © 2016년 ZOYI. All rights reserved.
+//
+
+#import <CommonCrypto/CommonCrypto.h>
