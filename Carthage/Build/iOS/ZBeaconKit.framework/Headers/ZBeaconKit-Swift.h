5797a3fe63061a27f25d5c25fc8b309edfe416d0
Sean <sean@zoyi.co>
2016-11-08 21:13:12 +0900

    New version build

--- Carthage/Build/iOS/ZBeaconKit.framework/Headers/ZBeaconKit-Swift.h
+++ Carthage/Build/iOS/ZBeaconKit.framework/Headers/ZBeaconKit-Swift.h
@@ -167,6 +167,7 @@ SWIFT_CLASS("_TtC10ZBeaconKit7Manager")
 + (void)setCustomerId:(NSString * _Nullable)value;
 - (nonnull instancetype)initWithEmail:(NSString * _Nonnull)email authToken:(NSString * _Nonnull)token brandId:(NSInteger)brandId OBJC_DESIGNATED_INITIALIZER;
 - (void)start;
+- (void)stop;
 @end
 
 
